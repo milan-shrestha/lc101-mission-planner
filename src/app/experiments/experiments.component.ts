0cd8f1c6f1fe737f004a30f165d4680a88a0beef
Milan Shrestha <milanshrestha17@hotmail.com>
2020-08-06 17:26:32 -0400

    Modified codes to allow users to add, edit and remove experiments

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[] = ['Mars soil sample', 'Plant growth in habitat', 'Human bone density'];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(experiment: string) {
+    if (this.experiments.includes(experiment)) {
+      alert('Experiment is already on the list.');
+      return;
+    }
+    this.experiments.push(experiment);
+  }
+
+  remove(experiment: string) {
+    let index = this.experiments.indexOf(experiment);
+    this.experiments.splice(index, 1);
+  }
+
+  experimentBeingEdited: string = null;
+
+  edit(experiment: string) {
+    this.experimentBeingEdited = experiment;
+ }
+
+  save(name: string, experiment: string) {
+    if (name === '') {
+      alert('Please enter experiment name.');
+      return;
+    }
+    if (this.experiments.includes(name)) {
+      alert('Experiment is already on the list.');
+      return;
+    }
+    let index = this.experiments.indexOf(experiment);
+    this.experiments[index] = name;
+    this.experimentBeingEdited = null;
+  }
 }
