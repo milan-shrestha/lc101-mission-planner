26d5488f0819bfb1ec775b70d5223bb3a830a87c
Milan Shrestha <milanshrestha17@hotmail.com>
2020-08-06 17:15:52 -0400

    Modified codes to allow users to add, edit and remove equipments

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipments: string[] = ['Habitat dome', 'Drones', 'Food containers', 'Oxygen tanks'];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(equipment: string) {
+    if (this.equipments.includes(equipment)) {
+      alert('Equipment is already on the list.');
+      return;
+    }
+    this.equipments.push(equipment);
+  }
+
+  remove(equipment: string) {
+    let index = this.equipments.indexOf(equipment);
+    this.equipments.splice(index, 1);
+  }
+
+  equipmentBeingEdited: string = null;
+
+  edit(equipment: string) {
+    this.equipmentBeingEdited = equipment;
+ }
+
+  save(name: string, equipment: string) {
+    if (name === '') {
+      alert('Please enter equipment name.');
+      return;
+    }
+    if (this.equipments.includes(name)) {
+      alert('Equipment is already on the list.');
+      return;
+    }
+    let index = this.equipments.indexOf(equipment);
+    this.equipments[index] = name;
+    this.equipmentBeingEdited = null;
   }
 
 }
